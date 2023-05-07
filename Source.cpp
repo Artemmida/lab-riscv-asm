2d8b95b570ccd6a09287e999d7b13aeec4a2842c
Artemmida <131971424+Artemmida@users.noreply.github.com>
2023-05-07 17:14:45 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+#define N 4
+#define M 4
+
+int sum_row(int* matrix, int  line)
+{
+	int sum = 0;
+	for (int i = 0; i < N; i++)
+	{
+		sum+=matrix[M*(line-1)+i];
+	}
+	return sum;
+}
+
+int main()
+{
+	int matrix[M][N] = { 0 };
+	for (int i = 0; i < M; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			matrix[i][j] = rand() % 10;
+			printf("%d\t", matrix[i][j]);
+		}
+		printf("\n");
+	}
+	
+	int line = 1;
+	int sum = sum_row(&matrix[0][0], line);
+	printf("\nSum of the row with number %d is %d \n", line, sum);
+	return 0;
+}
